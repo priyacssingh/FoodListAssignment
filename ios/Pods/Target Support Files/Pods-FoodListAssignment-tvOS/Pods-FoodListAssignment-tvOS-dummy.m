96e64e8171090875cf4c37981228edb45830eb05
Priya Singh <priya.singh@squarehealth.com>
2020-08-28 22:34:30 +0530

    initial commit

---
+++ ios/Pods/Target Support Files/Pods-FoodListAssignment-tvOS/Pods-FoodListAssignment-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FoodListAssignment_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_FoodListAssignment_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-FoodListAssignment/Pods-FoodListAssignment-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FoodListAssignment : NSObject
+@end
+@implementation PodsDummy_Pods_FoodListAssignment
+@end
